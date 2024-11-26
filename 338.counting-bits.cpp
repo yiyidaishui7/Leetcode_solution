bd460737807ad7bbd4476dd5108f09aa7cdfdf1e
aaakinc <yagkinc@163.com>
2024-11-26 18:05:57 +0800

    day7

---
+++ 338.counting-bits.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=338 lang=cpp
+ * @lcpr version=30204
+ *
+ * [338] 比特位计数
+ *
+ * https://leetcode.cn/problems/counting-bits/description/
+ *
+ * algorithms
+ * Easy (78.92%)
+ * Likes:    1354
+ * Dislikes: 0
+ * Total Accepted:    353.2K
+ * Total Submissions: 447.5K
+ * Testcase Example:  '2'
+ *
+ * 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans
+ * 作为答案。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：[0,1,1]
+ * 解释：
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：n = 5
+ * 输出：[0,1,1,2,1,2]
+ * 解释：
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= n <= 10^5
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 很容易就能实现时间复杂度为 O(n log n) 的解决方案，你可以在线性时间复杂度 O(n) 内用一趟扫描解决此问题吗？
+ * 你能不使用任何内置函数解决此问题吗？（如，C++ 中的 __builtin_popcount ）
+ *
+ *
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> res(n + 1, 0);
+        res[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+
+            // if i % 2 == 1,res[i] = res[i-1]+1;
+            // if i % 2 == 0,res[i] = res[i/2];
+            res[i] = (i % 2 == 0) ? res[i / 2] : (res[i - 1] + 1);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 5\n
+// @lcpr case=end
+
+ */
---
+++ 700.search-in-a-binary-search-tree.cpp
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=cpp
+ * @lcpr version=30204
+ *
+ * [700] 二叉搜索树中的搜索
+ *
+ * https://leetcode.cn/problems/search-in-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (78.68%)
+ * Likes:    485
+ * Dislikes: 0
+ * Total Accepted:    356.8K
+ * Total Submissions: 453.3K
+ * Testcase Example:  '[4,2,7,1,3]\n2'
+ *
+ * 给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+ *
+ * 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ * 输入：root = [4,2,7,1,3], val = 2
+ * 输出：[2,1,3]
+ *
+ *
+ * 示例 2:
+ *
+ * 输入：root = [4,2,7,1,3], val = 5
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数在 [1, 5000] 范围内
+ * 1 <= Node.val <= 10^7
+ * root 是二叉搜索树
+ * 1 <= val <= 10^7
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == nullptr)
+            return nullptr;
+        if (root->val == val)
+            return root;
+        if (root->val > val)
+            return searchBST(root->left, val);
+        else
+            return searchBST(root->right, val);
+        return nullptr;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [4,2,7,1,3]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,2,7,1,3]\n5\n
+// @lcpr case=end
+
+ */
